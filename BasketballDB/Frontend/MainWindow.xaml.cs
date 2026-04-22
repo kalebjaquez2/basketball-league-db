44f61cf69750584ef5c256a41ca9562a561f63b9
Brett <brettastockwell@gmail.com>
2026-04-22 08:45:16 -0500

    initial home screen with title add league button and displaying leagues that have already been added

--- BasketballDB/Frontend/MainWindow.xaml.cs
+++ BasketballDB/Frontend/MainWindow.xaml.cs
@@ -1,24 +1,56 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Frontend
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadLeagues();
+        }
+
+        private void LoadLeagues()
+        {
+            // TODO: Replace with real data from Backend
+            // Example: LeagueTilesControl.ItemsSource = LeagueRepository.GetAllLeagues();
+            var sampleLeagues = new[]
+            {
+                new { LeagueID = 1, LeagueName = "Adult League", Location = "Downtown Rec Center" },
+                new { LeagueID = 2, LeagueName = "Youth League", Location = "Downtown Rec Center" },
+                new { LeagueID = 3, LeagueName = "3v3 Summer League", Location = "Downtown Rec Center" }
+            };
+            LeagueTilesControl.ItemsSource = sampleLeagues;
+        }
+
+        private void ShowHomeScreen()
+        {
+            HomeScreen.Visibility = Visibility.Visible;
+            MainFrame.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowFrame()
+        {
+            HomeScreen.Visibility = Visibility.Collapsed;
+            MainFrame.Visibility = Visibility.Visible;
+        }
+
+        private void AddLeague_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Open Add League dialog/page
+            // Example: new AddLeagueWindow().ShowDialog();
+            // Then reload leagues after adding
+            // LoadLeagues();
+        }
+
+        private void LeagueTile_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Tag is int leagueId)
+            {
+                ShowFrame();
+                // TODO: MainFrame.Navigate(new SeasonsView(leagueId));
+            }
         }
     }
 }
